30112b597ccd6cd9c59236de42251bfec9f8f1ca
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-12-05 12:30:15 -0800

    feat: add other setup cmds

--- src/zq/setup.py
+++ src/zq/setup.py
@@ -1,8 +1,6 @@
 import os
 import click
 
-from zq.utils.my_win_env import MY_WIN_ENV
-
 @click.group()
 def setup():
     """Setup Commands group"""
@@ -11,6 +9,23 @@ def setup():
 @setup.command()
 def crack():
     """crack"""
+    from zq.utils.my_win_env import MY_WIN_ENV
     os.system(MY_WIN_ENV / 'scripts' / 'crack.ps1')
 
-    
+@setup.command()
+def debloat():
+    """debloat"""
+    from zq.utils.my_win_env import MY_WIN_ENV
+    os.system(MY_WIN_ENV / 'scripts' / 'debloat.ps1')
+
+@setup.command()
+def defaults():
+    """set defaults"""
+    from zq.utils.my_win_env import MY_WIN_ENV
+    os.system(MY_WIN_ENV / 'scripts' / 'setup.ps1')
+
+@setup.command()
+def devenv():
+    """setup development environment"""
+    from zq.utils.my_win_env import MY_WIN_ENV
+    os.system(MY_WIN_ENV / 'scripts' / 'devenv.ps1')
