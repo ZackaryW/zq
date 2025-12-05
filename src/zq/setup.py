992f9810a1d3ce053ab00d8bd028ceb489274890
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-12-05 12:33:38 -0800

    fix: type error

--- src/zq/setup.py
+++ src/zq/setup.py
@@ -10,22 +10,22 @@ def setup():
 def crack():
     """crack"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(MY_WIN_ENV / 'scripts' / 'crack.ps1')
+    os.system(str(MY_WIN_ENV / 'scripts' / 'crack.ps1'))
 
 @setup.command()
 def debloat():
     """debloat"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(MY_WIN_ENV / 'scripts' / 'debloat.ps1')
+    os.system(str(MY_WIN_ENV / 'scripts' / 'debloat.ps1'))
 
 @setup.command()
 def defaults():
     """set defaults"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(MY_WIN_ENV / 'scripts' / 'setup.ps1')
+    os.system(str(MY_WIN_ENV / 'scripts' / 'setup.ps1'))
 
 @setup.command()
 def devenv():
     """setup development environment"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(MY_WIN_ENV / 'scripts' / 'devenv.ps1')
+    os.system(str(MY_WIN_ENV / 'scripts' / 'devenv.ps1'))
