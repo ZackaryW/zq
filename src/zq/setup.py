955739ed92298b97576981f7b31d7eb9af29fab8
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-12-05 12:44:54 -0800

    fix: exec

--- src/zq/setup.py
+++ src/zq/setup.py
@@ -1,6 +1,8 @@
 import os
 import click
 
+from zq.utils.execute import execute_cmd
+
 @click.group(invoke_without_command=True)
 @click.option("--force-update", "-f", is_flag=True, help="Force update the my_win_env repository")
 @click.option("--reset", "-r", is_flag=True, help="Reset my_win_env to original state")
@@ -23,22 +25,21 @@ def setup(ctx, force_update, reset):
 def crack():
     """crack"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(str(MY_WIN_ENV / 'scripts' / 'crack.ps1'))
+    execute_cmd(str(MY_WIN_ENV / 'scripts' / 'crack.ps1'), watch_non_zero=False)
 
 @setup.command()
 def debloat():
     """debloat"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(str(MY_WIN_ENV / 'scripts' / 'debloat.ps1'))
-
+    execute_cmd(str(MY_WIN_ENV / 'scripts' / 'debloat.ps1'))
 @setup.command()
 def defaults():
     """set defaults"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(str(MY_WIN_ENV / 'scripts' / 'setup.ps1'))
+    execute_cmd(str(MY_WIN_ENV / 'scripts' / 'setup.ps1'), watch_non_zero=False)
 
 @setup.command()
 def devenv():
     """setup development environment"""
     from zq.utils.my_win_env import MY_WIN_ENV
-    os.system(str(MY_WIN_ENV / 'scripts' / 'devenv.ps1'))
+    execute_cmd(str(MY_WIN_ENV / 'scripts' / 'devenv.ps1'), watch_non_zero=False)
---
+++ src/zq/utils/execute.py
@@ -0,0 +1,17 @@
+
+
+def execute_cmd(command : str, watch_non_zero : bool = True):
+    # first try pwsh then fallback to powershell, then cmd
+    import os
+    try:
+        ret = os.system("pwsh -Command " + command)
+    except Exception:
+        try:
+            ret = os.system("powershell -Command " + command)
+        except Exception:
+            raise Exception("No PowerShell found in PATH")
+        
+    if ret != 0 and watch_non_zero:
+        raise Exception(f"Command failed with exit code {ret}: {command}")
+    
+    return ret
